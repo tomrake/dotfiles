016c7d762c8dbcbddca980f73c79a0499ce4167e
Tom Rake <zzzap1957@gmail.com>
2023-08-03 23:01:29 -0400

    Corrected only-emacs.

--- dot_emacs-profiles.el
+++ dot_emacs-profiles.el
@@ -1,2 +1,2 @@
 (("default" . ((user-emacs-directory . "~/Documents/Code/.emacs.d")))
- ("onlyemacs" .((user-emacs-directory . "~/Documents/Code/.standard-emacs.d"))))
+ ("onlyemacs" .((user-emacs-directory . "~/.standard-emacs.d"))))
