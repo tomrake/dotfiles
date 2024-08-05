dbd72ce53a3b13e9419379b3faaeba967dbe8d22
Tom Rake <zzzap1957@gmail.com>
2024-08-05 16:42:49 -0400

    Thined down .emacs-profiles.el.

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,9 +1,5 @@
 
-(("production" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/production/emacs.d")))
+(;("default" .  ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/production/emacs.d")))
+ ("production" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/production/emacs.d")))
  ("testing" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/testing/emacs.d"))) 
- ("devel" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/devel/emacs.d")))
- ("DESKER-DEFAULT" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/production/emacs.d")
-		      (env . (("USERINITCUSTOM" . "~/Documents/Code/Emacs-Configs/production/emacs.d/scripts/DESKER-DEFAULT-user-startup")))))
- 
- ("slime" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/slime/.emacs.d")))
- ("sly" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/sly/.emacs.d"))))
+ ("devel" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/devel/emacs.d"))))
