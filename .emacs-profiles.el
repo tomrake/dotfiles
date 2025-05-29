81cc0fa49684bc386e195c6c6136796a29f58df1
Tom Rake <zzzap1957@gmail.com>
2025-05-29 09:30:01 -0400

    Update .emacs-profiles.el

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -2,4 +2,6 @@
 (;("default" .  ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/production/emacs.d")))
  ("production" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/production/emacs.d")))
  ("testing" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/testing/emacs.d"))) 
- ("devel" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/devel/emacs.d"))))
+ ("devel" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/devel/emacs.d")))
+ ("lab" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/lab/emacs.d")))
+ ("vanilla" . ((user-emacs-directory . "~/Documents/Code/Emacs-Configs/vanilla/emacs.d"))))
