f768f9fe6c67df6f4417740aa7f12b6cf7d8b00d
Tom Rake <zzzap1957@gmail.com>
2025-05-29 20:58:43 -0400

    Add updated .quicklisp-user.lisp

--- .quicklisp-user.lisp
+++ .quicklisp-user.lisp
@@ -1,1 +1,11 @@
-/c/Users/zzzap/Documents/Code/.quicklisp-user.lisp
+;;;; Add project directories for quicklisp
+
+
+(let ((paths
+	'(#P"C:/Users/zzzap/Documents/Code/source-projects/"
+	  #P"C:/Users/zzzap/Documents/Code/source-projects/ACTIVE/")))
+;;;; Add any from paths which is not present in ql:*local-project-directories*
+  (dolist (p paths)
+    (when (and p (probe-file p) (not (member p ql:*local-project-directories*)))
+      (print (format t "Adding ~A to quicklisp local projects directory." p))
+      (setf ql:*local-project-directories* (cons p ql:*local-project-directories*)))))
